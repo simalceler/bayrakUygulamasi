33cf25f92051015cc9f3bd174a299e2eeec5525b
simalceler <celersimal@gmail.com>
2023-05-23 12:00:49 +0300

    git add.

--- BayrakUygulamasi/ViewController.swift
+++ BayrakUygulamasi/ViewController.swift
@@ -11,7 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        veritabaniKopyala()
+    }
+    
+    
+    func veritabaniKopyala() {
+        
+        let bundleYolu = Bundle.main.path(forResource: "bayrakquiz", ofType: ".sqlite")
+        
+        let hedefYol = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        let fileManager = FileManager.default
+        
+        let kopyalanacakYer = URL(fileURLWithPath: hedefYol).appendingPathComponent("bayrakquiz.sqlite")
+        
+        if fileManager.fileExists(atPath: kopyalanacakYer.path) {
+            print("Veritabanı zaten var.Kopyalamaya gerek yok.")
+        }else{
+            do {
+                try fileManager.copyItem(atPath: bundleYolu!, toPath: kopyalanacakYer.path)
+            }catch{
+                print(error)
+            }
+        }
     }
 
 
